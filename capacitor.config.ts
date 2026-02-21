bc5ced67bf62a03b1afc1cf32e904364ef682e8c
excellencior <turjob44@gmail.com>
2026-02-21 09:53:35 +0600

    Android apk splash screen inconsistency fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.costpilot.app',
   appName: 'CostPilot',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+      launchAutoHide: true,
+      backgroundColor: "#020617",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+    },
+  },
 };
 
 export default config;
